e46001dceadb5c213d351a172622f7593804d4ff
johnsonhabarugira <habarugirajohnson@gmail.com>
2024-08-29 10:35:57 +0200

    first commit

---
+++ banking_system.py
@@ -0,0 +1,15 @@
+balance = 300000
+action = input(f"choose action: check balance,deposit money, withdraw").strip().lower()
+if action == "check balance":
+    print(f"Your current balance is:{balance}")
+elif action =="deposit money":
+    deposited_amount = float(input(f"enter amount"))
+    balance += deposited_amount
+    print(f"you have deposited {deposited_amount}. you new balance is:{balance}")
+elif action == "withdraw":
+    withdraw_amount= float(input(f"enter amount to withdraw"))
+    if withdraw_amount > balance:
+        print(f"low balanc")
+    else:
+        balance -= withdraw_amount
+        print(f"you have withdrawn {withdraw_amount}. your new balance is{balance}")
---
+++ one.py
@@ -0,0 +1,10 @@
+print("num1")
+n1 =input()
+print("op")
+op= input()
+print("num2")
+n2 = input()
+if op == "+":
+    results =int(n1) + int(n2)
+    print(results)
+
