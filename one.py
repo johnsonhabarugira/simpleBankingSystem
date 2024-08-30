d09a1822943695dfa4b5f6976ba18da562855f3c
johnsonhabarugira <habarugirajohnson@gmail.com>
2024-08-30 09:17:51 +0200

    loop

--- one.py
+++ one.py
@@ -1,10 +1,3 @@
-print("num1")
-n1 =input()
-print("op")
-op= input()
-print("num2")
-n2 = input()
-if op == "+":
-    results =int(n1) + int(n2)
-    print(results)
-
+room = ('item1','item2','item3')
+for i in room:
+    print(i)
